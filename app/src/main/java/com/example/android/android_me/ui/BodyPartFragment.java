60d4fbeee6b8113f5d11eb4879972f9cc8eba907
Avi Gagel <avigagel@Avis-MacBook-Pro.local>
2018-02-17 18:28:09 +0200

    Create BodyPart Java Class for Fragment

---
+++ app/src/main/java/com/example/android/android_me/ui/BodyPartFragment.java
@@ -0,0 +1,37 @@
+package com.example.android.android_me.ui;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.example.android.android_me.R;
+import com.example.android.android_me.data.AndroidImageAssets;
+
+/**
+ * Created by avigagel on 17/02/2018.
+ */
+
+public class BodyPartFragment extends Fragment {
+
+
+    public BodyPartFragment() {
+    }
+
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.fragment_body_part, container,false);
+
+        ImageView headView = (ImageView) rootView.findViewById(R.id.body_part_image_view);
+        headView.setImageResource(AndroidImageAssets.getHeads().get(0));
+
+        return rootView;
+    }
+
+}
